c07fb935971d764872293071c0319ac57bfbb33e
d-alsar <doglasreyes290@gmail.com>
2024-11-17 20:25:05 -0600

    js file completed

---
+++ scripts/temples.js
@@ -0,0 +1,33 @@
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+/* ❔What does toggle mean?
+We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
+The CSS class rules will handle the different views, layouts, and displays.
+🗝️ JavaScript only applies the class value or not.
+*/
+
+const yearElement = document.querySelector("#currentYear");
+const currentYear = new Date().getFullYear();
+
+// This creates a copyright symbol element
+const copyrightSymbol = document.createTextNode("© ");
+
+// This adds the copyright symbol and year to the span element
+yearElement.appendChild(copyrightSymbol);
+yearElement.appendChild(document.createTextNode(currentYear));
+
+
+const today = new Date();
+const dayElement = document.querySelector("#lastModified");
+
+dayElement.innerHTML = `Last modification: <span class="highlight">${new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full"
+}).format(today)}</span>`;
